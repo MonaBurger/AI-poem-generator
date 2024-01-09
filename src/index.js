13ae1f182e1bf08b837964830c5f34c0f05c4ad2
MonaBurger <mona@software-lab.de>
2024-01-09 12:38:41 +0100

    improved the UX

--- src/index.js
+++ src/index.js
@@ -18,6 +18,10 @@ function generatePoem(event) {
     "You are a romantic poem expert and love to write short poems. Your mission is to write a poem with EXACTLY 4 lines in basic HTML and separate each line with a < /br>. Do not include a title to the poem. Make sure to follow the user instructions. Sign the poem with `Shecodes AI` inside a <strong> element at the END of the poem, always at the end, NOT - NEVER - at the beginning";
   let apiUrl = `https://api.shecodes.io/ai/v1/generate?prompt=${prompt}&context=${context}&key=${apiKey}`;
 
+  let poemElement = document.querySelector("#poem");
+  poemElement.classList.remove("hidden");
+  poemElement.innerHTML = `<div class="blink">⏳ Generating an english poem about ${instructionsInput.value}...<blink/>`;
+
   console.log("Generating poem");
   console.log(`Prompt: ${prompt}`);
   console.log(`Context: ${context}`);
